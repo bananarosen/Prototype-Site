48a7407a73a6f92e85a359fdf1cd08956f3706e4
Hannah Rosen <hannahfayerosen@gmail.com>
2016-10-09 16:28:58 -0400

    PUI Assignment 3

---
+++ js/myscript.js
@@ -0,0 +1,50 @@
+// Declaring global variables
+var taskentered = "";
+var familymember = "";
+
+//hiding the add task view part of the page that will be useful later
+$(document).ready(function() {
+    $("#add_task_container").hide();
+});
+
+//switching to the add task view
+$("#add_task").click(function(){
+    $("#family_list_container").hide();
+    //resetting the task value field
+    $("#task_field").val("");
+    $("#add_task_container").show(); 
+});
+
+/*storing the name of the family member who was clicked on 
+and also providing visual feedback of who was selected */
+$(".person").click(function(){
+    familymember = ($(this).attr("id"));
+    //removing the person_selected class if someone has previously been selected
+    $(".person").removeClass("person_selected");
+    $(this).addClass("person_selected");
+});
+
+//Storing the new task as a string and adding the task to the list and setting delete and complete functions for the new tasks
+$("#complete_add").click(function(){
+    taskentered = ($("#task_field").val());
+    $("#add_task_container").hide();
+    $("#family_list_container").show();
+    $("#family_list").append('<li class="list-group-item">' + '<span class="label"><img src="img/'+ familymember+ '.jpg" width="20" height="20"></span>' + taskentered + '<span class="glyphicon glyphicon-remove float_right"></span><span class="glyphicon glyphicon-ok float_right"></span></li>');
+    createOnClickListeners(); 
+});
+
+
+// function to hold the delete and complete tasks 
+function createOnClickListeners() {
+$(".glyphicon-remove").click(function(){
+    $(this).parent().fadeOut(500);
+});
+
+$(".glyphicon-ok").click(function(){
+   $(this).parent().addClass("task_done");
+});    
+}
+
+// calling the delete and complete function when the javaScript loads
+createOnClickListeners();
+
